a83356a7a68cc430c7fc004e2e78f357de74e743
sameeram <mgjaye@gmail.com>
2016-08-31 16:40:27 +0530

    Fixed an error in sample

--- restservice/asset.js
+++ restservice/asset.js
@@ -17,158 +17,166 @@
  *
  */
 asset.manager = function(ctx) {
-    //If extension is moved to another folder (tenant support), this path need to be changed
-    //JIRA:https://wso2.org/jira/browse/STORE-613
-    var configs = require("/extensions/assets/soapservice/config/properties.json");
     var tenantAPI = require('/modules/tenant-api.js').api;
-    /**
-     * The function augments the provided query to include published state information
-     * @param  {[type]} query [description]
-     * @return {[type]}       The provided query object
-     */
-    var buildPublishedQuery = function(query) {
-        //Get all of the published assets
-        var publishedStates = ctx.rxtManager.getPublishedStates(ctx.assetType) || [];
-        //Determine if there are any published states
-        if (publishedStates.length == 0) {
-            return query;
+    var setCustomAssetAttributes = function (asset, userRegistry) {
+        var wadlUrl = asset.attributes.interface_wadl;
+        if (wadlUrl != null) {
+            try {
+                var resource = userRegistry.registry.get(wadlUrl);
+                var wadlContent = getInterfaceTypeContent(resource);
+                var ComparatorUtils = Packages.org.wso2.carbon.governance.comparator.utils.ComparatorUtils;
+                var comparatorUtils = new ComparatorUtils();
+                var mediaType = "application/wadl+xml";
+                try {
+                    wadlContent = comparatorUtils.prettyFormatText(wadlContent, mediaType);
+                } catch (ex) {
+
+                }
+                asset.wadlContent = wadlContent;
+            } catch (e) {
+                asset.wadlContent = "";
+            }
         }
-        //If there is no query then build a new one
-        if (!query) {
-            query = {};
+        var swaggerUrl = asset.attributes.interface_swagger;
+        if (swaggerUrl != null) {
+            try {
+                var resource = userRegistry.registry.get(swaggerUrl);
+                var swaggerContent = getInterfaceTypeContent(resource);
+                asset.swaggerContent = swaggerContent;
+            } catch (e) {
+                asset.swaggerContent = "";
+            }
         }
-        //TODO: Even though an array is sent in only the first search value is accepted
-        query.lcState = [publishedStates[0]];
-        return query;
+    }; 
+
+    var setDependencies = function(genericArtifact, asset ,userRegistry) {
+        var dependencyArtifacts = genericArtifact.getDependencies();
+        asset.dependencies = getAssociations(dependencyArtifacts, userRegistry);
     };
+
+    var setDependents = function(genericArtifact, asset, userRegistry) {
+        var dependentArtifacts = genericArtifact.getDependents();
+        asset.dependents = getAssociations(dependentArtifacts, userRegistry);
+    };
+
     var getRegistry = function(cSession) {
-        var tenantDetails = tenantAPI.createTenantAwareAssetResources(cSession, {
-            type: ctx.assetType
-        });
-        if ((!tenantDetails) && (!tenantDetails.am)) {
+        var tenantDetails = tenantAPI.createTenantAwareAssetResources(cSession,{type:ctx.assetType});
+        if((!tenantDetails)&&(!tenantDetails.am)) {
             log.error('The tenant-api was unable to create a registry instance by resolving tenant details');
             throw 'The tenant-api  was unable to create a registry instance by resolving tenant details';
         }
         return tenantDetails.am.registry;
     };
-    var setCustomAssetAttributes = function(asset, userRegistry) {
-        var interfaceUrl = asset.attributes.interface_wsdlURL;
-        if (interfaceUrl != null) {
-            try {
-                var resource = userRegistry.registry.get(interfaceUrl);
-                var wsdlContent = getInterfaceTypeContent(resource);
-                asset.wsdlContent = wsdlContent;
-                var wsdlUUID = getInterfaceTypeUUID(resource);
-                asset.wsdl_uuid = wsdlUUID;
-            } catch (e) {
-                asset.wsdl_uuid = "";
-                asset.wsdlContent = "";
-            }
-        }
-    };
-    var getInterfaceTypeContent = function(resource) {
+
+    var getInterfaceTypeContent = function (resource) {
         var ByteArrayInputStream = Packages.java.io.ByteArrayInputStream;
         var content = resource.getContent();
         var value = '' + new Stream(new ByteArrayInputStream(content));
-        //this is wsdlcontent.
         return value;
     };
-    var getInterfaceTypeUUID = function(resource) {
-        var wsdlUUID = resource.getUUID();
-        return wsdlUUID;
-    };
-    var getAssociations = function(genericArtifacts, userRegistry) {
+
+    var getAssociations = function(genericArtifacts, userRegistry){
         //Array to store the association names.
         var associations = [];
         if (genericArtifacts != null) {
             for (var index in genericArtifacts) {
                 var deps = {};
-                //extract the association name via the path.
                 var path = genericArtifacts[index].getPath();
+                var resource = userRegistry.registry.get('/_system/governance' + path);
+                var mediaType = resource.getMediaType();
+                var name = genericArtifacts[index].getQName().getLocalPart();
+                var govUtils = Packages.org.wso2.carbon.governance.api.util.GovernanceUtils;
+                var keyName = govUtils.getArtifactConfigurationByMediaType(getRegistry(ctx.session).registry, mediaType).getKey();
                 var subPaths = path.split('/');
-                var associationTypePlural = subPaths[2];
-                var associationName = subPaths[subPaths.length - 1];
-                var resource = userRegistry.registry.get(configs.depends_asset_path_prefix + path);
+                var associationName = name;
                 var associationUUID = resource.getUUID();
                 var associationVersion = genericArtifacts[index].getAttribute("overview_version");
-                if (!associationVersion && (subPaths.length - 2) > -1) {
+                // This is only for wadls and swaggers which have correct storage path
+                if(!associationVersion && (subPaths.length - 2)>-1){
                     associationVersion = subPaths[subPaths.length - 2]
                 }
                 deps.associationName = associationName;
-                deps.associationType = associationTypePlural.substring(0, associationTypePlural.lastIndexOf('s'));
+                deps.associationType = keyName;
                 deps.associationUUID = associationUUID;
+                deps.associationPath = resource.getPath();
                 deps.associationVersion = associationVersion;
-                if (deps.associationType == "wsdl") {
+
+                if(deps.associationType == "wadl") {
+                    associations.push(deps);
+                }
+                if(deps.associationType == "swagger") {
                     associations.push(deps);
                 }
             }
         }
         return associations;
     };
-    var setDependencies = function(genericArtifact, asset, userRegistry) {
-        //get dependencies of the artifact.
-        var dependencyArtifacts = genericArtifact.getDependencies();
-        asset.dependencies = getAssociations(dependencyArtifacts, userRegistry);
-    };
-    var setDependents = function(genericArtifact, asset, userRegistry) {
-        var dependentArtifacts = genericArtifact.getDependents();
-        asset.dependents = getAssociations(dependentArtifacts, userRegistry);
-    };
+
     return {
-        //due to a bug needed to replicate the 'search' method. JIRA:https://wso2.org/jira/browse/STORE-561
         search: function(query, paging) {
-            //query = buildPublishedQuery(query);--commented this inorder to let anystate
-            //to be visible in store.
             var assets = this._super.search.call(this, query, paging);
             return assets;
         },
         get: function(id) {
-            //TODO: support services added through WSDL, once multiple lifecycle is supported.
             var asset = this._super.get.call(this, id);
             var userRegistry = getRegistry(ctx.session);
             try {
                 setCustomAssetAttributes(asset, userRegistry);
-            } catch (e) {}
+            } catch (e){}
+
             //get the GenericArtifactManager
             var rawArtifact = this.am.manager.getGenericArtifact(id);
             try {
                 setDependencies(rawArtifact, asset, userRegistry);
-            } catch (e) {}
+            } catch (e){}
             try {
                 setDependents(rawArtifact, asset, userRegistry);
-            } catch (e) {}
+            } catch (e){
+
+            }
+
             return asset;
         }
     };
 };
-asset.configure = function() {
+
+asset.configure = function () {
     return {
         meta: {
             ui: {
-                icon: 'fw fw-soap',
-                iconColor: 'orange'
+                icon: 'fw fw-rest-service',
+                iconColor: 'purple'
             },
             isDependencyShown: true,
-            isDiffViewShown: false
+            isDiffViewShown:false
         }
     }
 };
-asset.renderer = function(ctx) {
+
+asset.renderer = function(ctx){
     return {
-        pageDecorators: {
-            downloadPopulator: function(page) {
+        pageDecorators:{
+            downloadPopulator:function(page){
                 //Populate the links for downloading content RXTs
-                if (page.meta.pageName === 'details') {
+                if(page.meta.pageName === 'details'){
                     var config = require('/config/store.js').config();
-                    var pluralType = 'wsdls';
-                    var domain = require('carbon').server.tenantDomain({
-                        tenantId: ctx.tenantId
-                    });
-                    page.assets.downloadMetaData = {};
-                    if (page.assets.wsdlContent) {
-                        page.assets.downloadMetaData.enabled = true;
-                        page.assets.downloadMetaData.downloadFileType = 'WSDL';
-                        page.assets.downloadMetaData.url = config.server.https + '/governance/' + pluralType + '/' + page.assets.wsdl_uuid + '/content?tenant=' + domain;
+                    var pluralType = 'wadls'; //Assume it is a WADl
+                    var domain = require('carbon').server.tenantDomain({tenantId:ctx.tenantId});
+                    page.assets.downloadMetaData = {}; 
+                    page.assets.downloadMetaData.enabled = false;
+                    var dependencies = page.assets.dependencies || [];
+                    var downloadFile = dependencies.filter(function(item){
+                        return ((item.associationType == 'wadl')||(item.associationType == 'swagger'));
+                    })[0];
+                    if(downloadFile){
+                      var typeDetails = ctx.rxtManager.getRxtTypeDetails(downloadFile.associationType);
+                      page.assets.downloadMetaData.enabled = true;  
+                      page.assets.downloadMetaData.downloadFileType = typeDetails.singularLabel.toUpperCase();
+                      pluralType = typeDetails.pluralLabel.toLowerCase();
+                      page.assets.downloadMetaData.url = config.server.https+'/governance/'+pluralType+'/'+downloadFile.associationUUID+'/content?tenant='+domain;          
+                      if(downloadFile.associationType == 'swagger'){
+                        page.assets.downloadMetaData.swaggerUrl = '/pages/swagger?path='+downloadFile.associationPath;
+                      }
                     }
                 }
             },
